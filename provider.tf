498ee033dd9a172d4df9c701a8dd78ec06a6d2ce
ASHOKDEVOPSSOFT-2024 <ashokganisoftitcarrer8094@gmail.com>
2024-10-30 14:52:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,5 +9,8 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
+  #profile = "default"
+  access_key = "AKIA4IM3HDZHRYQB4R6A"
+  secret_key = "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"
   region = "us-east-1"
 }
