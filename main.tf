0d2af2ed147ba498915985060c50ac3ea43173a0
ASHOKDEVOPSSOFT-2024 <ashokganisoftitcarrer8094@gmail.com>
2024-10-30 15:09:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,10 +34,3 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   to_port           = 443
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
