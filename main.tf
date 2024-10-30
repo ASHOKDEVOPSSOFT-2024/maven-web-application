c7a41fd8bc11e4a605b60cb8f28de3ba9506ec86
ASHOKDEVOPSSOFT-2024 <ashokganisoftitcarrer8094@gmail.com>
2024-10-30 15:03:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "main-vpc" {
+resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
@@ -7,7 +7,7 @@ resource "aws_vpc" "main-vpc" {
   }
 }
 
-resource "aws_subnet" "main-subnet" {
+resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
